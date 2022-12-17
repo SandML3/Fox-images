742079e92d640e3e1627ea34df71f1e12101c19d
SandML3 <SandML3@users.noreply.github.com>
2022-12-17 07:41:41 +0000

    deploy: 668b72ab2b26a19bf44326f52f0f0b05dcbd4d59

---
+++ _dist_/index.js
@@ -0,0 +1,33 @@
+import { registerImage } from "./lazy.js";
+
+const random = (max, min) => Math.floor(Math.random() * (max - min) + min);
+
+const createImageNode = () => {
+  const image = document.createElement("img");
+  image.dataset.src = `https://randomfox.ca/images/${random(123, 1)}.jpg`;
+  image.className = "mx-auto";
+  image.title = "Fox image";
+  image.alt = "Fox image";
+  image.width = "320";
+
+  const container = document.createElement("div");
+  container.className = "p-4";
+  container.append(image);
+
+  return container;
+};
+
+const newImage = createImageNode();
+newImage.firstChild.src = `https://randomfox.ca/images/${random(123, 1)}.jpg`;
+const mountNode = document.querySelector("#images");
+
+const button = document.querySelector("button");
+const handlerClick = () => {
+  const newImage = createImageNode();
+  mountNode.append(newImage);
+  registerImage(newImage);
+};
+
+button.addEventListener("click", handlerClick);
+
+mountNode.append(newImage);
