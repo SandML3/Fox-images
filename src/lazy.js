d4cabe3ba05d72234a4c64d3acc892f2c74c02bd
SandMl3 <sandramartinlop@gmail.com>
2022-12-17 09:17:53 +0100

    Add a console total images and loaded images counter

--- src/lazy.js
+++ src/lazy.js
@@ -2,6 +2,10 @@ const isIntersecting = (entry) => {
   return entry.isIntersecting;
 };
 
+const isVisible = (entry) => {
+  return entry.src;
+};
+
 const loadImage = (entry) => {
   const container = entry.target;
   const image = container.firstChild;
@@ -11,12 +15,17 @@ const loadImage = (entry) => {
   observer.unobserve(entry.target);
 };
 
-//
 const observer = new IntersectionObserver((allEntries) => {
   allEntries.filter(isIntersecting).forEach(loadImage);
+
+  console.log(
+    "Número total de imágenes: ",
+    document.querySelectorAll("img").length,
+    "Número de imágenes cargadas: ",
+    [...document.querySelectorAll("img")].filter(isVisible).length
+  );
 });
 
-//
 export const registerImage = (imagen) => {
   observer.observe(imagen);
 };
